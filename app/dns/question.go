de14e9af641d7701578cef6a8bf06153d83b59ad
Sebastian Haba <sebastian@haba.net.pl>
2025-04-18 16:48:49 +0200

    Add DNS header and question handling to support DNS messages
    Implemented `Header` and `Question` structs with parsing and serialization methods for DNS messages. Updated the `Message` struct to include these new components, enabling full support for DNS packet construction and parsing.

---
+++ app/dns/question.go
@@ -0,0 +1,104 @@
+package dns
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Question struct {
+	QNAME  string
+	QTYPE  uint16
+	QCLASS uint16
+}
+
+func (q *Question) String() string {
+	return fmt.Sprintf("Question{QNAME: %s, QTYPE: %d, QCLASS: %d}", q.QNAME, q.QTYPE, q.QCLASS)
+}
+
+func (q *Question) ToBytes() []byte {
+	var bytes []byte
+	bytes = append(bytes, domainNameToBytes(q.QNAME)...)
+	bytes = append(bytes, byte(q.QTYPE>>8), byte(q.QTYPE&0xFF))
+	bytes = append(bytes, byte(q.QCLASS>>8), byte(q.QCLASS&0xFF))
+
+	return bytes
+
+}
+
+func parseQuestion(data []byte, offset int) (Question, int) {
+	question := Question{}
+	endOffset := offset
+
+	name, newOffset := parseDomainName(data, offset)
+	question.QNAME = name
+	endOffset = newOffset
+
+	if len(data) < endOffset+4 {
+		return question, endOffset
+	}
+
+	question.QTYPE = uint16(data[endOffset])<<8 | uint16(data[endOffset+1])
+	endOffset += 2
+
+	question.QCLASS = uint16(data[endOffset])<<8 | uint16(data[endOffset+1])
+	endOffset += 2
+
+	return question, endOffset
+
+}
+
+func parseDomainName(data []byte, offset int) (string, int) {
+	if offset >= len(data) {
+		return "", offset
+	}
+
+	var name string
+	position := offset
+	length := int(data[position])
+
+	for length > 0 {
+		position++
+
+		if position+length > len(data) {
+			break
+		}
+
+		label := string(data[position : position+length])
+		name += label + "."
+
+		position += length
+
+		if position >= len(data) {
+			break
+		}
+
+		length = int(data[position])
+	}
+
+	if length == 0 {
+		position++
+	}
+
+	return name, position
+}
+
+func domainNameToBytes(domain string) []byte {
+	var bytes []byte
+
+	if len(domain) > 0 && domain[len(domain)-1] == '.' {
+		domain = domain[:len(domain)-1]
+	}
+
+	if domain == "" {
+		return []byte{0}
+	}
+
+	labels := strings.Split(domain, ".")
+	for _, label := range labels {
+		bytes = append(bytes, byte(len(label)))
+		bytes = append(bytes, []byte(label)...)
+	}
+
+	bytes = append(bytes, 0)
+	return bytes
+}
